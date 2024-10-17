f4cacce0559d93d2e693c192aeeb0cbbd896a6a4
89JBWebb <89JBWebb@gmail.com>
2024-10-17 16:30:55 -0400

    removed tutorial

--- GUI.py
+++ GUI.py
@@ -1,7 +1,9 @@
 import tkinter
 m = tkinter.Tk()
-'''
-widgets are added here
-'''
+from PIL import ImageTk, Image
+
 m.title("Euchre GUI")
+img = ImageTk.PhotoImage(Image.open("./images/30.gif"))
+label1 = tkinter.Label(image=img)
+label1.place(x=0,y=0)
 m.mainloop()
